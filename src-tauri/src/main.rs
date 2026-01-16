ad5a80e8fc2f5e3105d327d3e608761e25c0c1fd
Ameya Shenoy <shenoy.ameya@gmail.com>
2026-01-16 23:21:16 +0530

    feat: add --version
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use clap::Parser;
+
+/// HyperWhisper - The most productive speech to text app for Linux
+#[derive(Parser, Debug)]
+#[command(name = "hyperwhisper")]
+#[command(author, version, about, long_about = None)]
+struct Args {}
+
 fn main() {
+    // Parse CLI arguments - clap handles --version and --help automatically
+    let _args = Args::parse();
+
+    // Run the Tauri application
     hyperwhisper_lib::run()
 }
