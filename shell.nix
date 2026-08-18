ce4a4eefddd1bfb38c2660897ef1f4b1b667dbc0
Ameya Shenoy <shenoy.ameya@gmail.com>
2026-08-18 15:12:01 +0530

    fix: deleted dep
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

--- shell.nix
+++ shell.nix
@@ -56,8 +56,6 @@ in pkgs.mkShell {
     gst_all_1.gst-plugins-ugly
     # Plugins to reuse ffmpeg to play almost every video format
     gst_all_1.gst-libav
-    # Support the Video Audio (Hardware) Acceleration API
-    gst_all_1.gst-vaapi
   ];
 
   # Set ALSA library path for cpal
